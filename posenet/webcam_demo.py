107bc1f7f08a88d9c24be0a6482b588ce4a76f4d
dhruvi29 <drdoshi29@gmail.com>
2020-06-12 15:56:45 +0530

    basic_game_control

--- posenet/webcam_demo.py
+++ posenet/webcam_demo.py
@@ -15,6 +15,7 @@ import time
 import argparse
 import numpy as np
 import posenet
+import keyboard
 ###################################### keypoint co-ords line 52 ##############################
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=int, default=101)
@@ -27,29 +28,82 @@ args = parser.parse_args()
 
 ################################### Difining grid pattern and location of center of left and right shoulder 33333333333333333333333
 gidd = np.array((args.cam_width//3,args.cam_width//1.5,args.cam_height//3,args.cam_height//1.5))
-def grid(center,overlay_image):
+def grid(center,overlay_image,row,column):
     # print(gidd)
-    row='center'
-    column='center'
+    row_copy=row
+    column_copy=column
     if(center[0]>gidd[0] and center[0]<gidd[1]):
-        row='center'
-    elif(center[0]<gidd[0]):
-        row='left'
-    elif(center[0]>gidd[1]):
-        row='right'
-    if(center[1]>gidd[2] and center[1]<gidd[3]):
         column='center'
+    elif(center[0]<gidd[0]):
+        column='left'
+    elif(center[0]>gidd[1]):
+        column='right'
+    if(center[1]>gidd[2] and center[1]<gidd[3]):
+        row='center'
     elif(center[1]<gidd[2]):
-        column='up'
+        row='up'
     elif(center[1]>gidd[3]):
-        column='bottom'
+        row='bottom'
     # print("row:",row)
     # print("column:",column)
     cv2.putText(overlay_image, row, (50,430), cv2.FONT_HERSHEY_SIMPLEX,3, (0,255,255), 5, cv2.LINE_AA) 
     cv2.putText(overlay_image, column, (50,490), cv2.FONT_HERSHEY_SIMPLEX,3, (0,255,255), 5, cv2.LINE_AA)
-    return None
+    if(row_copy!=row):
+        print('row change')
+        # if(row_copy=='up'):
+        #     if(row=='center'):
+        #         keyboard.press_and_release('s')
+        #         print(row)
+        #     if(row=='bottom'):
+        #         keyboard.press_and_release('s')
+        #         print(row)
+        if(row_copy=='center'):
+            if(row=='up'):
+                keyboard.press_and_release('w')
+                print(row)
+            elif(row=='bottom'):
+                keyboard.press_and_release('s')
+                print(row)
+        # elif(row_copy=='bottom'):
+        #     if(row=='center'):
+        #         keyboard.press_and_release('w')
+        #         print(row)
+        #     elif(row=='up'):
+        #         keyboard.press_and_release('w')
+        #         print(row)
+    if(column_copy!=column):
+        print('column change')
+        if(column_copy=='right'):
+            if(column=='center'):
+                keyboard.press_and_release('a')
+                print(column)
+            if(column=='left'):
+                keyboard.press_and_release('a')
+                keyboard.press_and_release('a')
+                print(column)
+        if(column_copy=='center'):
+            if(column=='left'):
+                keyboard.press_and_release('a')
+                print(column)
+            elif(column=='right'):
+                keyboard.press_and_release('d')
+                print(column)
+        elif(column_copy=='left'):
+            if(column=='center'):
+                keyboard.press_and_release('d')
+                print(column)
+            elif(column=='right'):
+                keyboard.press_and_release('d')
+                keyboard.press_and_release('d')
+                print(column)
+    return row,column
 
+
+row='center'
+column='center'
 def main():
+    global row
+    global column
     with tf.Session() as sess:
         model_cfg, model_outputs = posenet.load_model(args.model, sess)
         output_stride = model_cfg['output_stride']
@@ -85,24 +139,30 @@ def main():
                 min_pose_score=0.15)
 
             keypoint_coords *= output_scale
-            # print(keypoint_coords)
+            # print(keypoint_scores)
 #################### Get keypoint Co-ordinates #######################################################
             # TODO this isn't particularly fast, use GL for drawing and display someday...
             overlay_image = posenet.draw_skel_and_kp(
                 display_image, pose_scores, keypoint_scores, keypoint_coords,
                 min_pose_score=0.15, min_part_score=0.1)
 ################## Pose Classificaton ##################################################################
-            keypoint_coords =  keypoint_coords.squeeze()
-            # keypoint_coords = np.array(keypoint_coords,dtype=np.int64)
-            left_shoulder_y = keypoint_coords[5,0]
-            left_shoulder_x = keypoint_coords[5,1]
-            right_shoulder_y = keypoint_coords[6,0]
-            right_shoulder_x = keypoint_coords[6,1]
-            centre_x = int(right_shoulder_x+left_shoulder_x)//2
-            centre_y = int(right_shoulder_y+left_shoulder_y)//2
-            centre = (centre_x,centre_y)
-            cv2.circle(overlay_image,centre,10,(255,255,255),-1)
-            grid(centre,overlay_image)
+            if(keypoint_scores[0][5]>0.1 and keypoint_scores[0][6]>0.1 and pose_scores[0]>0.15):
+                keypoint_coords =  keypoint_coords.squeeze()
+                # keypoint_coords = np.array(keypoint_coords,dtype=np.int64)
+                left_shoulder_y = keypoint_coords[5,0]
+                left_shoulder_x = keypoint_coords[5,1]
+                right_shoulder_y = keypoint_coords[6,0]
+                right_shoulder_x = keypoint_coords[6,1]
+                centre_x = int(right_shoulder_x+left_shoulder_x)//2
+                centre_y = int(right_shoulder_y+left_shoulder_y)//2
+                centre = (centre_x,centre_y)
+                cv2.circle(overlay_image,centre,10,(255,255,255),-1)
+                row,column=grid(centre,overlay_image,row,column)
+                
+            cv2.line(overlay_image,(430,0),(430,720),(255,255,255),5)
+            cv2.line(overlay_image,(860,0),(860,720),(255,255,255),5)
+            cv2.line(overlay_image,(0,240),(1290,240),(255,255,255),5)
+            cv2.line(overlay_image,(0,480),(1290,480),(255,255,255),5)
             
             overlay_image=cv2.imshow('posenet', overlay_image)
             frame_count += 1
