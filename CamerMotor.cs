7b6a42ab0c4dd5d2cd06e703ed81d2943f02948c
sarah-nisar <64372302+sarah-nisar@users.noreply.github.com>
2020-06-12 19:30:39 +0530

    Add files via upload

---
+++ CamerMotor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerMotor : MonoBehaviour
+{
+    private Transform lookAt;
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+
+    private float transition = 0.0f;
+    private float animationDuration = 3.0f;
+    private Vector3 animationOffset = new Vector3(0, 5, 5);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAt=GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAt.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = lookAt.position + startOffset;
+
+        //X
+        moveVector.x = 0;
+
+
+        //Y
+        moveVector.y = Mathf.Clamp(moveVector.y, 3, 5);
+
+        if(transition>1.0f)
+        {
+            transform.position = moveVector;
+        }
+        else
+        {
+            //Animation at the start of the game
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += Time.deltaTime * 1 / animationDuration;
+            transform.LookAt(lookAt.position + Vector3.up);
+        }
+
+
+    }
+}
---
+++ DeathMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    
+    public Text scoreText;
+    public Image backgroundImg;
+
+    private bool isShowed = false;
+
+    private float transition=0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isShowed)
+            return;
+
+        transition += Time.deltaTime;
+        backgroundImg.color = Color.Lerp(new Color(0, 0, 0, 0), Color.black, transition);
+    }
+
+    public void ToggleEndMenu(float score)
+    {
+        gameObject.SetActive(true);
+        scoreText.text = ((int)score).ToString();
+        isShowed = true;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
+
