6d1ec187ccc2503ad70d4d98a006fa4dad3af587
SAtacker <shreyasatre16@gmail.com>
2020-06-22 21:21:02 +0530

    .

--- train.py
+++
@@ -1,9 +0,0 @@
-import c3d
-import os
-
-path = os.path.abspath(os.getcwd())
-path += r"\Human Eva\S1\Mocap_Data\Box_1.c3d"
-with open(path, 'rb') as handle:
-    reader = c3d.Reader(handle)
-    for i, (points, analog) in enumerate(reader.read_frames()):
-        print('Frame {}: {}'.format(i, points.round(2)))
